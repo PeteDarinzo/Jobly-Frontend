2e20067743622a69c04476560afd078cfc22c79a
Peter Darinzo <birel44@yahoo.com>
2022-01-27 18:36:49 -0800

    add filtering capability

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CompanyCard from "./CompanyCard";
 import { Link } from "react-router-dom";
 import "./CompanyList.css";
@@ -9,16 +9,22 @@ import JoblyApi from "./Api";
 
 const CompanyList = ({ companies }) => {
 
+  const [companyList, setCompanyList] = useState(companies)
+
+  /** Take the filter from the search form, return companies mathing filter query
+   *  getAllCompanies takes an object and sets it as data in the request
+   *  "name" is the filter to search companies by name
+   */
   async function filterCompanies(term) {
-    const res = await JoblyApi.getAllCompanies({ query: term });
-    console.log(res);
+    const res = await JoblyApi.getAllCompanies({ name: term });
+    setCompanyList(res)
   }
 
   return (
     <div>
       <h1>All Companies:</h1>
       <CompanySearchForm filter={filterCompanies} />
-      {companies.map(company => (
+      {companyList.map(company => (
         <Link to={`companies/${company.handle}`} className="CompanyListLink" key={uuid()}>
           <CompanyCard name={company.name} description={company.description} />
         </Link>
